c58c5e0b6a7a28cc3430bf65a898bf624c8e1e21
Vanessa Farraye <vanessafarraye@Vanessas-MacBook-Air-2.local>
2015-06-03 17:42:28 -0700

    ready

--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,23 @@
 require_relative 'car'
 
-class Race
+class Race 
   #write Race class code here
+  attr_accessor :cars
+  def initialize
+  	@cars=[]
+  	car1 = Car.new
+  	car2 = Car.new
+  	@cars << car1 << car2
+  	car1.speed = rand(0..100)
+  	car2.speed = rand(0..100)
+  end
+  def winner
+  	return @cars[0] if @cars[0].speed > @cars[1].speed 
+  	@cars[1]
+  end
+  def loser
+  	return @cars[0] if @cars[0].speed < @cars[1].speed
+  	@cars[1]
+  end
+
 end
